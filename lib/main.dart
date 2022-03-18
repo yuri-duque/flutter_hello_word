f46727624ed722ddfa165a3f77a157b57d2d91c9
Yuri Duque <yurithielmann83@gmail.com>
2022-03-17 23:01:48 -0300

    alterando state da pagina ao clicar

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,32 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(),
-      home: Container(
-        child: Center(child: Text("teste")),
-      ),
+    return MaterialApp(theme: ThemeData.light(), home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: GestureDetector(
+        child: Text('contador: ${counter}'),
+        onTap: () {
+          setState(() {
+            counter++;
+          });
+        },
+      )),
     );
   }
 }
